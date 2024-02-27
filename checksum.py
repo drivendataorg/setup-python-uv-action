0af4ad9d6def155df16ef652fa2ab65d1d4e1f92
Jay Qi <jayqi@users.noreply.github.com>
2024-02-27 12:03:06 -0500

    Sort and formatting

--- checksum.py
+++ checksum.py
@@ -5,15 +5,15 @@ import textwrap
 
 def main():
     dep_checksums = []
-    for dep in sys.argv[1:]:
+    for dep in sorted(sys.argv[1:]):
         # In general, dep is a glob
-        for path in Path().glob(dep):
+        for path in sorted(Path().glob(dep)):
             dep_checksums.append((path, hashlib.md5(path.read_bytes()).hexdigest()))
     dep_checksums_str = textwrap.indent(
         "\n".join([f"{path} {checksum}" for path, checksum in dep_checksums]),
         "    "
     )
-    sys.stderr.write(f"Dependency checksums: {dep_checksums_str}\n")
+    sys.stderr.write(f"Dependency checksums:\n{dep_checksums_str}\n")
     overall_checksum = hashlib.md5(dep_checksums_str.encode()).hexdigest()
     sys.stderr.write(f"Overall checksum: {overall_checksum}\n")
     sys.stdout.write(overall_checksum)
