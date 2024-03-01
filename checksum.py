d2bc5e795c004b873fc10ada7b31088df3efbd13
Jay Qi <jayqi@users.noreply.github.com>
2024-03-01 10:50:04 -0500

    base64 encode final checksum

--- checksum.py
+++ checksum.py
@@ -1,7 +1,9 @@
+import base64
 import hashlib
 from pathlib import Path
 import sys
 import textwrap
+
 
 def main():
     dep_checksums = []
@@ -9,14 +11,18 @@ def main():
         # In general, dep is a glob
         for path in sorted(Path().glob(dep)):
             dep_checksums.append((path, hashlib.md5(path.read_bytes()).hexdigest()))
-    dep_checksums_str = textwrap.indent(
-        "\n".join([f"{path} {checksum}" for path, checksum in dep_checksums]),
-        "    "
+    dep_checksums_str = "\n".join(
+        [f"{path} {checksum}" for path, checksum in dep_checksums]
     )
-    sys.stderr.write(f"Dependency checksums:\n{dep_checksums_str}\n")
-    overall_checksum = hashlib.md5(dep_checksums_str.encode()).hexdigest()
+    sys.stderr.write(
+        f"Dependency checksums:\n{textwrap.indent(dep_checksums_str, '    ')}\n"
+    )
+    overall_checksum = base64.urlsafe_b64encode(
+        hashlib.md5(dep_checksums_str.encode()).digest()
+    )
     sys.stderr.write(f"Overall checksum: {overall_checksum}\n")
     sys.stdout.write(overall_checksum)
+
 
 if __name__ == "__main__":
     main()
